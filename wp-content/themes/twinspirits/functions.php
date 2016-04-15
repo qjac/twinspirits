32e241013ce9caf2412044b8eb31d5ad0ba94a22
Sarah German <sarah@advomatic.com>
2016-04-14 21:55:39 -0500

    Banner image styles

--- wp-content/themes/twinspirits/functions.php
+++ wp-content/themes/twinspirits/functions.php
@@ -5,3 +5,23 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for post thumbnails
+ *
+ * @since Twenty Sixteen 1.0
+ *
+ * @param array $attr Attributes for the image markup.
+ * @param int   $attachment Image attachment ID.
+ * @param array $size Registered image size or flat array of height and width dimensions.
+ * @return string A source size value for use in a post thumbnail 'sizes' attribute.
+ */
+function twinspirits_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
+  if ( 'post-thumbnail' === $size ) {
+    is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
+    ! is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1400px) 88vw, 1400px';
+  }
+  return $attr;
+}
+add_filter( 'wp_get_attachment_image_attributes', 'twinspirits_post_thumbnail_sizes_attr', 20 , 3 );
