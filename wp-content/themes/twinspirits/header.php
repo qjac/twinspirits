3e7704d8cc89cfd4dc60f6280f4d5518b0216754
Sarah German <sarah@advomatic.com>
2016-05-14 16:51:34 -0500

    More header fixes

--- wp-content/themes/twinspirits/header.php
+++ wp-content/themes/twinspirits/header.php
@@ -19,6 +19,8 @@
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
 	<?php wp_head(); ?>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700|Open+Sans:700|Source+Serif+Pro|Roboto+Slab:700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 </head>
 
 <body <?php body_class(); ?>>
@@ -58,19 +60,6 @@
 							</nav><!-- .main-navigation -->
 						<?php endif; ?>
 
-						<?php if ( has_nav_menu( 'social' ) ) : ?>
-							<nav id="social-navigation" class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentysixteen' ); ?>">
-								<?php
-								wp_nav_menu( array(
-									'theme_location' => 'social',
-									'menu_class'     => 'social-links-menu',
-									'depth'          => 1,
-									'link_before'    => '<span class="screen-reader-text">',
-									'link_after'     => '</span>',
-								) );
-								?>
-							</nav><!-- .social-navigation -->
-						<?php endif; ?>
 					</div><!-- .site-header-menu -->
 				<?php endif; ?>
 			</div><!-- .site-header-main -->
