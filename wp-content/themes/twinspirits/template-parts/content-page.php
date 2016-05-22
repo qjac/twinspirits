d19f31d683de3951e8b100a02d9963ab7e1b36d9
Sarah German <sarah@advomatic.com>
2016-05-22 11:52:21 -0500

    Put image back onto the homepage

--- wp-content/themes/twinspirits/template-parts/content-page.php
+++ wp-content/themes/twinspirits/template-parts/content-page.php
@@ -14,9 +14,11 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php twentysixteen_post_thumbnail(); ?>
 	</header><!-- .entry-header -->
-	<?php endif; ?>
+		<?php endif; ?>
 
-	
+	<?php if (is_front_page()) {
+		twentysixteen_post_thumbnail();
+	} ?>
 
 	<div class="entry-content">
 		<?php
