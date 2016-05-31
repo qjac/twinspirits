48f8432b5e55310039ceb08c1d10190bfdaae0f3
Sarah German <sarah@advomatic.com>
2016-05-31 13:11:10 -0500

    Fix address in footer.

--- wp-content/themes/twinspirits/footer.php
+++ wp-content/themes/twinspirits/footer.php
@@ -27,7 +27,7 @@
 					?>
 				</nav><!-- .social-navigation -->
 			<?php endif; ?>
-			<div class="address">2591 Central Ave NE<br>Minneapolis, MN 55418</div>
+			<div class="address">2931 Central Ave NE<br>Minneapolis, MN 55418</div>
 			<p class="copyright">&copy; <?php echo date('Y'); ?> Twin Spirits Distillery. All rights reserved.</p>
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
