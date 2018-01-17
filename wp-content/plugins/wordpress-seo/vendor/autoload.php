147b1709ebffc8bd1e7984daa5213891f1b27df3
Sarah German <sarah@sarahgerman.com>
2018-01-17 21:16:28 +0000

    Update Yoast SEO.

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit4bece919afb2ef52ec98278a50c6d52d::getLoader();
+return ComposerAutoloaderInit76cfc7b06c72274c2d811e383db95e3e::getLoader();
