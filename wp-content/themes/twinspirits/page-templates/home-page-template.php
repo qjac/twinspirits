f666c2780429013e472fe0db94fdb5b85fa9b7f7
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-02-20 21:33:08 -0600

    HOMEPAGE! WooHoo! bomb styles

--- wp-content/themes/twinspirits/page-templates/home-page-template.php
+++ wp-content/themes/twinspirits/page-templates/home-page-template.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Home page
+ * Template Name: Home Page
  *
  * @package WordPress
  * @subpackage twinspirits
@@ -10,6 +10,24 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+		<div class="home-title">
+			<h1><?php bloginfo( 'name' ); ?></h1>
+		</div> 
+		<div class="home-block-one" style="background-image: url(<?php the_field('first_image'); ?>);">
+				<div class="home-text-block">
+					<h2><?php the_field('first_title'); ?></h2>
+					<div class="home-text-body"><?php the_field('first_text_block'); ?></div>
+				</div> <!-- end .home-text-block -->
+			<!-- </div>  end .home-image -->
+
+		</div>
+		<div class="home-block-two" style="background-image: url(<?php the_field('second_image'); ?>);">
+				<div class="home-text-block">
+					<h2><?php the_field('second_title'); ?></h2>
+					<div class="home-text-body"><?php the_field('second_text_block'); ?></div>
+				</div> <!-- end .home-text-block -->
+			<!-- </div> end .home-image -->
+		</div>
 
 		<?php
 		// Start the loop.
