d463254db10155f82e3d3487d6e4e0fe43539ce1
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-02-23 04:33:29 -0600

    staff page styles. random styling fixes.

--- wp-content/themes/twinspirits/template-parts/content-single.php
+++ wp-content/themes/twinspirits/template-parts/content-single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template part for displaying content
+ * The template part for displaying single posts
  *
  * @package WordPress
  * @subpackage Twenty_Sixteen
@@ -8,29 +8,24 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-	<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
-		<header class="entry-header">
-			<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
-	</header><!-- .entry-header -->
-	<?php endif; ?>
+<article id="post-<?php the_ID();?>" <?php post_class(); ?>>
 
-	
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_excerpt(); ?>
+
 	<?php twentysixteen_post_thumbnail(); ?>
 
-
 	<div class="entry-content">
-
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-		<?php twentysixteen_excerpt(); ?>
+		
 		<?php
-		// for grabbing staff title on team page
-			if(get_field('title'))
+		if(get_field('title'))
 			{ echo '<h3 class="team-title">' . get_field('title') . '</h3>'; }
-			?>
+		
+			the_content();
 
-			<?php
 			// for grabbing recipe ingredients on recipe pages
 			if(get_field('ingredients'))
 			{ echo '<div class="recipe-ingredients">' . get_field('ingredients') . '</div>'; }
@@ -38,13 +33,7 @@
 			// for grabbing recipe instructions on recipe pages
 			if(get_field('instructions'))
 			{ echo '<div class="recipe-instructions">' . get_field('instructions') . '</div>'; }
-			?>
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-				get_the_title()
-			) );
+
 
 			wp_link_pages( array(
 				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
@@ -54,6 +43,10 @@
 				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
 				'separator'   => '<span class="screen-reader-text">, </span>',
 			) );
+
+			if ( '' !== get_the_author_meta( 'description' ) ) {
+				get_template_part( 'template-parts/biography' );
+			}
 		?>
 	</div><!-- .entry-content -->
 
@@ -71,4 +64,5 @@
 			);
 		?>
 	</footer><!-- .entry-footer -->
+
 </article><!-- #post-## -->
